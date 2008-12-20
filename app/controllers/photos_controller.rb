1487a06ecd5b07206ea1d67ab1c7c4abb523fe25
Samuel Esposito <samuel.esposito@gmail.com>
2008-12-20 16:36:18 +0100

    Update upload photo action

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,10 +1,15 @@
 class PhotosController < ApplicationController
 
   def upload_photo
+    puts params.inspect
     @albums = Album.find :all, :order => "title"
     @photo = Photo.new(params[:photo])
     if request.post?
-      @photo.save
+      if params[:add_album]
+        redirect_to :controller => :album, :action => :add_album
+      else
+        @photo.save
+      end
     end
   end
 
